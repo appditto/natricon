3627ee96ac8d1b3071067a6a5624643b781077c1
yekta <ngyekta@gmail.com>
2020-05-25 21:11:28 +0300

    Stylesheet in head tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,14 @@ import Navbar from "~/components/Navbar.vue";
 export default {
   components: {
     Navbar
+  },
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://use.typekit.net/ypc1kli.css"
+      }
+    ]
   }
 };
 </script>
