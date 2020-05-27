a01422f5274e333edf52003d3220669526300b2c
yekta <ngyekta@gmail.com>
2020-05-27 16:59:40 +0300

    Donation model additions

--- components/OpensourceAndFree.vue
+++ components/OpensourceAndFree.vue
@@ -38,8 +38,8 @@
       <div class="w-full flex flex-row justify-center bg-white relative">
         <!-- Donate Dropdown -->
         <div
-          :class="isDropdownOpen?'h-108 border-black shadow-lightPink':'h-0 border-transparent'"
-          class="w-full md:w-144 absolute flex flex-col items-center max-w-128 bg-white border-4 transition-all duration-300 ease-out overflow-hidden mt-5 rounded-lg px-6 md:px-12"
+          :class="isDropdownOpen?'scale-y-100 border-black shadow-lightPink':'scale-y-0 border-transparent'"
+          class="w-full md:w-144 absolute flex flex-col items-center max-w-128 bg-white border-4 transition-all transform origin-top duration-300 ease-out overflow-hidden mt-5 rounded-lg px-2 md:px-12 z-50"
         >
           <!-- Go Back Button -->
           <button
@@ -48,32 +48,32 @@
             @click="resetDonation()"
           >go back</button>
           <div
-            :class="isDropdownOpen?'opacity-100':'opacity-0' "
-            class="flex flex-col justify-center items-center duration-700 ease-out py-8"
+            :class="isDropdownOpen?'opacity-100 duration-1000':'opacity-0 duration-150' "
+            class="w-full flex flex-col justify-center items-center ease-out pt-8 pb-6"
           >
             <img
-              v-if="donationAmount<=2"
-              class="w-40 h-40"
-              :src="require('~/assets/images/gifs/NatriconDonatePhase1.gif')"
-              alt="Natricon Donate 1"
-            />
-            <img
               v-if="donationAmount>2 && donationAmount<=10"
-              class="w-40 h-40"
+              class="w-32 h-32"
               :src="require('~/assets/images/gifs/NatriconDonatePhase2.gif')"
               alt="Natricon Donate 2"
             />
             <img
-              v-if="donationAmount>10 && donationAmount<100"
-              class="w-40 h-40"
+              v-else-if="donationAmount>10 && donationAmount<100"
+              class="w-32 h-32"
               :src="require('~/assets/images/gifs/NatriconDonatePhase3.gif')"
               alt="Natricon Donate 3"
             />
             <img
-              v-if="donationAmount>100"
-              class="w-40 h-40"
+              v-else-if="donationAmount>=100"
+              class="w-32 h-32"
               :src="require('~/assets/images/gifs/NatriconDonatePhase4.gif')"
               alt="Natricon Donate 4"
+            />
+            <img
+              v-else
+              class="w-32 h-32"
+              :src="require('~/assets/images/gifs/NatriconDonatePhase1.gif')"
+              alt="Natricon Donate 1"
             />
             <div class="flex flex-col items-center">
               <!-- Donate Amount Buttons -->
@@ -82,47 +82,49 @@
                 class="flex flex-row flex-wrap justify-center items-center my-4"
               >
                 <button
+                  @mouseover="donationAmount=2"
+                  @mouseleave="customNanoAmountModel?donationAmount=customNanoAmountModel:donationAmount=2"
                   @click="initiateDonationFor(2)"
                   class="btn w-32 font-medium text-lg bg-black text-white hover:text-lightPink btn-sm-shadow-lightPink rounded-lg px-3 pt-1 pb-3 mt-5 mx-3"
                 >2 nano</button>
                 <button
                   @mouseover="donationAmount=10"
-                  @mouseleave="donationAmount=2"
+                  @mouseleave="customNanoAmountModel?donationAmount=customNanoAmountModel:donationAmount=2"
                   @click="initiateDonationFor(10)"
                   class="btn w-32 font-medium text-lg bg-black text-white hover:text-lightPink btn-sm-shadow-lightPink rounded-lg px-3 pt-1 pb-3 mt-5 mx-3"
                 >10 nano</button>
                 <button
                   @mouseover="donationAmount=20"
-                  @mouseleave="donationAmount=2"
+                  @mouseleave="customNanoAmountModel?donationAmount=customNanoAmountModel:donationAmount=2"
                   @click="initiateDonationFor(20)"
                   class="btn w-32 font-medium text-lg bg-black text-white hover:text-lightPink btn-sm-shadow-lightPink rounded-lg px-3 pt-1 pb-3 mt-5 mx-3"
                 >20 nano</button>
               </div>
               <!-- Custom Amount Input Group -->
-              <form v-if="!isDonationInitiated" class="my-4">
-                <label class="text-xl font-bold mx-3" for="nanoAmount">custom amount</label>
-                <br />
+              <form
+                v-if="!isDonationInitiated"
+                class="w-full md:w-64 flex flex-col justify-center px-3 my-5"
+              >
+                <label class="w-full text-xl font-bold" for="customNanoAmount">custom amount</label>
                 <input
-                  class="w-64 text-lg font-medium border-black border-2 px-4 pt-1 pb-2 rounded-lg mt-2 mx-1"
+                  :class="inputError?'border-red text-red':'border-black'"
+                  class="w-full text-lg font-medium border-2 px-4 pt-1 pb-2 rounded-lg my-1 transition-colors duration-200 ease-out"
                   type="number"
-                  ref="nanoAmount"
-                  id="nanoAmount"
-                  name="nanoAmount"
+                  ref="customNanoAmount"
+                  id="customNanoAmount"
+                  name="customNanoAmount"
                   placeholder="enter amount"
+                  v-model="customNanoAmountModel"
+                  @input="inputChange()"
                 />
                 <button
-                  class="btn text-lg font-medium border-black hover:text-lightPink hover:border-lightPink border-2 bg-black text-white pt-1 pb-2 px-6 rounded-lg mx-1"
+                  @click.prevent="customAmountAction()"
+                  class="w-full btn text-lg font-medium border-black hover:text-lightPink hover:border-lightPink border-2 bg-black text-white pt-1 pb-2 px-6 rounded-lg my-1"
                 >donate</button>
               </form>
               <!-- QR Code for the Donation -->
-              <div class="flex flex-row justify-center items-center m-4">
-                <qrcode-vue
-                  class="m-4"
-                  v-if="isDonationInitiated"
-                  :value="qrValue"
-                  :size="qrSize"
-                  level="Q"
-                ></qrcode-vue>
+              <div v-if="isDonationInitiated" class="flex flex-row justify-center items-center m-4">
+                <qrcode-vue class="m-4" :value="qrValue" :size="qrSize" level="Q"></qrcode-vue>
                 <div class="flex flex-col m-4">
                   <h5 class="text-lg">scan to donate</h5>
                   <h4 class="text-2xl font-bold break-all">{{donationAmount}} nano</h4>
@@ -162,7 +164,8 @@ export default {
       qrValue:
         "nano:nano_1natrium1o3z5519ifou7xii8crpxpk8y65qmkih8e8bpsjri651oza8imdd?amount=2000000000000000000000000000000",
       qrSize: 150,
-      donationAmount: 2
+      donationAmount: 2,
+      inputError: false
     };
   },
   components: {
@@ -190,6 +193,19 @@ export default {
       this.qrValue =
         "nano:nano_1natrium1o3z5519ifou7xii8crpxpk8y65qmkih8e8bpsjri651oza8imdd?amount=2000000000000000000000000000000";
       this.donationAmount = 2;
+    },
+    customAmountAction() {
+      if (this.donationAmount >= 0.000001 && this.donationAmount <= 10000000) {
+        this.initiateDonationFor(this.donationAmount);
+      } else {
+        this.inputError = true;
+      }
+    },
+    inputChange() {
+      if (this.inputError) {
+        this.inputError = false;
+      }
+      this.donationAmount = this.$refs.customNanoAmount.value;
     }
   }
 };
--- pages/documentation/index.vue
+++ pages/documentation/index.vue
@@ -140,7 +140,7 @@ export default {
         },
         { rel: "icon", sizes: "32x32", href: "/favicon-32x32.png" },
         { rel: "icon", sizes: "16x16", href: "/favicon-16x16.png" },
-        { rel: "manifest", href: "/site.manifest" }
+        { rel: "manifest", href: "/site.webmanifest" }
       ]
     };
   }
