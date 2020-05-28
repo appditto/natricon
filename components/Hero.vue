eda132a4aa9d2d036a2f27b5353f4d803a94c8bd
yekta <ngyekta@gmail.com>
2020-05-28 18:46:14 +0300

    Generator initial setup

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full flex flex-col items-center pt-6 pb-8 md:pt-12">
+  <div class="w-full flex flex-col items-center pt-6 pb-8 md:pt-12 z-50">
     <div class="n-container flex flex-col items-center px-5">
       <h1 class="text-4xl md:text-5xl text-center leading-none">
         meet your
@@ -10,8 +10,10 @@
       </h1>
       <h2 class="text-xl md:text-2xl text-center mt-3">like you've never seen before.</h2>
       <button
-        class="bg-black text-white hover:text-cyan btn-shadow-cyan font-medium text-2xl rounded-full px-16 pt-1 pb-3 mt-5"
-      >let's meet</button>
+        :class="isGeneratorOpen?'bg-cyan text-black btn-shadow-black':'bg-black text-white hover:text-cyan btn-shadow-cyan'"
+        @click="isGeneratorOpen=!isGeneratorOpen"
+        class="font-medium text-2xl rounded-full px-16 pt-1 pb-3 mt-5"
+      >{{isGeneratorOpen?"close":"let's meet"}}</button>
     </div>
     <img
       class="w-full h-auto mt-12 md:hidden"
@@ -23,14 +25,103 @@
       :src="require('~/assets/images/illustrations/hero-tablet.svg')"
       alt="Hero Tablet"
     />
-    <img
-      class="w-full h-auto mt-12 hidden lg:block"
-      :src="require('~/assets/images/illustrations/hero-desktop.svg')"
-      alt="Hero Desktop"
-    />
+    <div class="w-full flex flex-row justify-center h-auto mt-12 hidden lg:block relative">
+      <!-- Generator -->
+      <div
+        ref="generatorInside"
+        :class="isGeneratorOpen?'scale-100 opacity-100 duration-500':'scale-0 opacity-0 duration-200'"
+        class="w-108 h-108 flex flex-col justify-center items-center rounded-full bg-white shadow-xl mx-auto -mt-8 transform transition-all ease-out absolute generator z-50"
+      >
+        <!-- Nano Address Form Group -->
+        <form class="w-full flex flex-col justify-center px-12">
+          <input
+            :class="inputError?'border-red text-red':'border-black'"
+            class="w-full text-xl font-medium border-2 px-4 pt-1 pb-2 rounded-lg my-1 transition-colors duration-200 ease-out"
+            type="text"
+            ref="nanoAddress"
+            id="nanoAddress"
+            name="nanoAddress"
+            v-model="nanoAddress"
+            placeholder="enter your address"
+          />
+          <button
+            @click.prevent="generateNatricon()"
+            class="w-full btn text-xl font-medium border-black hover:text-cyan hover:border-cyan border-2 bg-black text-white pt-1 pb-2 px-6 rounded-lg mt-1"
+          >go!</button>
+        </form>
+      </div>
+      <!--  -->
+      <img
+        class="w-full h-auto"
+        :src="require('~/assets/images/illustrations/hero-background-desktop.svg')"
+        alt="Hero Background Desktop"
+      />
+      <img
+        :class="isGeneratorOpen?'-ml-84 hero-desktop-open':'hero-desktop'"
+        class="h-auto absolute top-0 left-0 transition-all duration-500 ease-out"
+        :src="require('~/assets/images/illustrations/hero-left-desktop.svg')"
+        alt="Hero Left Desktop"
+        ref="heroLeftDesktop"
+      />
+      <img
+        :class="isGeneratorOpen?'-mr-84 hero-desktop-open':'hero-desktop'"
+        class="h-auto absolute top-0 right-0 transition-all duration-500 ease-out"
+        :src="require('~/assets/images/illustrations/hero-right-desktop.svg')"
+        alt="Hero Right Desktop"
+        ref="heroRightDestop"
+      />
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      isGeneratorOpen: false,
+      nanoAddress: "",
+      inputError: false
+    };
+  },
+  methods: {
+    openGenerator() {
+      this.isGeneratorOpen = true;
+      this.$refs.heroLeftDesktop.style.marginRight = "0rem";
+    },
+    async generateNatricon() {
+      let ref = this;
+      const getNatriconResult = async () => {
+        try {
+          return await this.$axios.get(
+            "https://natricon.com/api/v1/nano?address=" + ref.nanoAddress
+          );
+        } catch (e) {
+          console.error(e);
+        }
+      };
+      const natriconResult = await getNatriconResult();
+      this.$refs.generatorInside.innerHTML = natriconResult.data;
+    }
+  }
+};
+</script>
 <style scoped>
+.btn {
+  transition: all 0.2s ease-out;
+  transform: scale(1);
+}
+.btn:hover {
+  transform: scale(0.95);
+}
+.generator {
+  left: 50%;
+  margin-left: -13.5rem;
+}
+.hero-desktop {
+  width: 55.5%;
+}
+.hero-desktop-open {
+  width: 50%;
+}
 .btn-shadow-cyan {
   box-shadow: -0.3rem 0.4rem 0rem 0rem#66ffff;
   transition: all 0.2s ease-out;
@@ -38,6 +129,15 @@
 }
 .btn-shadow-cyan:hover {
   box-shadow: 0rem 0rem 0rem 0rem#66ffff;
+  transform: scale(0.95);
+}
+.btn-shadow-black {
+  box-shadow: -0.3rem 0.4rem 0rem 0rem#000000;
+  transition: all 0.2s ease-out;
+  transform: scale(1);
+}
+.btn-shadow-black:hover {
+  box-shadow: 0rem 0rem 0rem 0rem#000000;
   transform: scale(0.95);
 }
 .line-cyan::after {
@@ -52,6 +152,9 @@
   margin-right: auto;
   margin-top: -0.6rem;
   background-color: #66ffff;
+  z-index: -1;
+}
+.bg-line {
   z-index: -1;
 }
 </style>
