03eef1454df79f1f000da387242588384adccf2e
BB <bbedward@gmail.com>
2021-07-24 18:41:33 -0400

    621

--- server/controller/nano.go
+++ server/controller/nano.go
@@ -194,7 +194,7 @@ func (nc NanoController) CheckMissedCallbacks() {
 	// Check history
 	historyResponse, err := nc.RPCClient.MakeAccountHistoryRequest(
 		nc.DonationAccount,
-		10,
+		621,
 	)
 	if err != nil {
 		glog.Errorf("Error occured checking donation account history %s", err)
